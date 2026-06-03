dd87b7274a1686c4b6ddf8ac8e21badb94f6185f
Ian Acuña <47308950+ki3anecromancer@users.noreply.github.com>
2026-06-03 14:06:42 -0300

    feat(config): se agregó configuración CORS

--- cfp-mapa/src/main/java/com/cfp/mapa/security/config/SecurityConfig.java
+++ cfp-mapa/src/main/java/com/cfp/mapa/security/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.cfp.mapa.security.config;
 
 import com.cfp.mapa.security.jwt.JwtAuthenticationEntryPoint;
 import com.cfp.mapa.security.jwt.JwtFilter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,6 +17,9 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +30,14 @@ public class SecurityConfig {
   private final JwtFilter jwtFilter;
   private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
+  @Value("${app.frontend-url}")
+  private String frontendUrl;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
     http
+        .cors(cors -> cors.configurationSource(corsConfigurationSourcer()))
         .csrf(AbstractHttpConfigurer::disable)
 
         .exceptionHandling(handling -> handling
@@ -57,5 +66,21 @@ public class SecurityConfig {
       throws Exception {
 
     return authConfig.getAuthenticationManager();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSourcer() {
+
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.setAllowedOrigins(List.of(frontendUrl));
+
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
